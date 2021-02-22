7d233abd606ba33bf9bbda63821d69fcb2c5c40a
UltraFaceguy <kattreinkyle@gmail.com>
2021-02-22 10:49:25 -0500

    Trying more video options

--- dumb-template/src/components/MainBanner/MainBanner.js
+++ dumb-template/src/components/MainBanner/MainBanner.js
@@ -6,7 +6,12 @@ export const MainBanner = () => {
 
     return (
         <div className="mainBanner">
-            <video className="videoBg" playsInline autoPlay muted loop src="https://s1.webmshare.com/yNvno.webm"/>
+            <video className="videoBg" playsInline autoPlay muted loop
+                   src="https://www.videovor.com/download/?token=598a068536f41483ec6bbc8aa6af9649de9b0017&vk=h0tAOQIKEbMjM3ldzFlXvLGzmgM3rW4xTINpu80OHkk&fn=Donald+Trump+-+Never+Come+Down+%28Original%29"
+                   poster="https://www.wallpapertip.com/wmimgs/2-27681_minecraft-wallpaper-4k-pc.jpg"
+            >
+                NO VIDEO 4 U
+            </video>
             <div className="bannerTitle">JET FUEL</div>
             <div className="bannerSubtitle">can't melt steel beems</div>
             <CopyWidget/>
