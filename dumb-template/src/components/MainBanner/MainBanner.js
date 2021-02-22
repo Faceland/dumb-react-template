53bde13251a4ef515a1bdcfd451b666f5e1b9ab4
UltraFaceguy <kattreinkyle@gmail.com>
2021-02-22 11:35:21 -0500

    Banner and widget font responsiveness

--- dumb-template/src/components/MainBanner/MainBanner.js
+++ dumb-template/src/components/MainBanner/MainBanner.js
@@ -7,7 +7,7 @@ export const MainBanner = () => {
     return (
         <div className="mainBanner">
             <video className="videoBg" playsInline autoPlay muted loop
-                   src="https://www.videovor.com/download/?token=598a068536f41483ec6bbc8aa6af9649de9b0017&vk=h0tAOQIKEbMjM3ldzFlXvLGzmgM3rW4xTINpu80OHkk&fn=Donald+Trump+-+Never+Come+Down+%28Original%29"
+                   src="https://webmshare.com/download/GxM6A"
                    poster="https://www.wallpapertip.com/wmimgs/2-27681_minecraft-wallpaper-4k-pc.jpg"
             >
                 NO VIDEO 4 U
