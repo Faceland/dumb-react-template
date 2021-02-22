12a198e636dbe82b65197ffe20ecff0b436f81ca
UltraFaceguy <kattreinkyle@gmail.com>
2021-02-22 03:52:03 -0500

    uhhhhhhhhhhhhhhhh :v

--- dumb-template/src/components/MainBanner/MainBanner.js
+++ dumb-template/src/components/MainBanner/MainBanner.js
@@ -6,6 +6,7 @@ export const MainBanner = () => {
 
     return (
         <div className="mainBanner">
+            <video className="videoBg" playsInline autoPlay muted loop src="https://s1.webmshare.com/yNvno.webm"/>
             <div className="bannerTitle">JET FUEL</div>
             <div className="bannerSubtitle">can't melt steel beems</div>
             <CopyWidget/>
