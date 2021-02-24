3cdbf2ff2d991daa13e6982d4f7c556a8223997c
UltraFaceguy <kattreinkyle@gmail.com>
2021-02-24 14:49:38 -0500

    breaking landingpage body into text and images

--- dumb-template/src/App.js
+++ dumb-template/src/App.js
@@ -45,130 +45,112 @@ function Home() {
             <div className="bodySection">
                 <div className="bodyPadding">
                     <Row>
-                        <Col md>
-                            <BodyPanel>
-                                <h5>mememe</h5>
-                                <div>According to all known laws of aviation, there is no way a bee should be able to
-                                    fly. Its wings are too small to get its fat little body off the ground. The bee, of
-                                    course, flies anyway because bees don't care what humans think is impossible.
-                                    Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and yellow!
-                                    Let's shake it up a little. Barry! Breakfast is ready! Ooming! Hang on a second.
-                                    Hello? - Barry? - Adam? - Oan you believe this is happening? - I can't. I'll pick
-                                    you up. Looking sharp. Use the stairs. Your father paid good money for those. Sorry.
-                                    I'm excited. Here's the graduate. We're very proud of you, son. A perfect report
-                                    card, all B's. Very proud. Ma! I got a thing going here. - You got lint on your
-                                    fuzz. - Ow! That's me! - Wave to us! We'll be in row 118,000. - Bye! Barry, I told
-                                    you, stop flying in the house! - Hey, Adam. - Hey, Barry. - Is that fuzz gel? - A
-                                    little. Special day, graduation. Never thought I'd make it. Three days grade school,
-                                    three days high school. Those were awkward. Three days college. I'm glad I took a
-                                    day and hitchhiked around the hive. You did come back different.
-                                </div>
-                            </BodyPanel>
-                        </Col>
-                        <Col md>
-                            <BodyPanel>
-                                <h5>mememe</h5>
-                                <div>According to all known laws of aviation, there is no way a bee should be able to
-                                    fly. Its wings are too small to get its fat little body off the ground. The bee, of
-                                    course, flies anyway because bees don't care what humans think is impossible.
-                                    Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and yellow!
-                                    Let's shake it up a little. Barry! Breakfast is ready! Ooming! Hang on a second.
-                                    Hello? - Barry? - Adam? - Oan you believe this is happening? - I can't. I'll pick
-                                    you up. Looking sharp. Use the stairs. Your father paid good money for those. Sorry.
-                                    I'm excited. Here's the graduate. We're very proud of you, son. A perfect report
-                                    card, all B's. Very proud. Ma! I got a thing going here. - You got lint on your
-                                    fuzz. - Ow! That's me! - Wave to us! We'll be in row 118,000. - Bye! Barry, I told
-                                    you, stop flying in the house! - Hey, Adam. - Hey, Barry. - Is that fuzz gel? - A
-                                    little. Special day, graduation. Never thought I'd make it. Three days grade school,
-                                    three days high school. Those were awkward. Three days college. I'm glad I took a
-                                    day and hitchhiked around the hive. You did come back different.
-                                </div>
-                            </BodyPanel>
-                        </Col>
+                        <BodyPanel>
+                            <Row>
+                                <Col md>
+                                    <img src="https://i.imgur.com/vSD8DGq.gif" alt="regretz"/>
+                                </Col>
+                                <Col md>
+                                    <h5>mememe</h5>
+                                    <div>According to all known laws of aviation, there is no way a bee should be able
+                                        to
+                                        fly. Its wings are too small to get its fat little body off the ground. The bee,
+                                        of
+                                        course, flies anyway because bees don't care what humans think is impossible.
+                                        Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and
+                                        yellow!
+                                        Let's shake it up a little. Barry! Breakfast is ready! Ooming! Hang on a second.
+                                        Hello? - Barry? - Adam? - Oan you believe this is happening? - I can't. I'll
+                                        pick
+                                        you up. Looking sharp. Use the stairs. Your father paid good money for those.
+                                        Sorry.
+                                        I'm excited. Here's the graduate. We're very proud of you, son. A perfect report
+                                        card, all B's. Very proud. Ma! I got a thing going here. - You got lint on your
+                                        fuzz. - Ow! That's me! - Wave to us! We'll be in row 118,000. - Bye! Barry, I
+                                        told
+                                        you, stop flying in the house! - Hey, Adam. - Hey, Barry. - Is that fuzz gel? -
+                                        A
+                                        little. Special day, graduation. Never thought I'd make it. Three days grade
+                                        school,
+                                        three days high school. Those were awkward. Three days college. I'm glad I took
+                                        a
+                                        day and hitchhiked around the hive. You did come back different.
+                                    </div>
+                                </Col>
+                            </Row>
+                        </BodyPanel>
                     </Row>
                     <Row>
-                        <Col md>
-                            <BodyPanel>
-                                <h5>mememe</h5>
-                                <div>According to all known laws of aviation, there is no way a bee should be able to
-                                    fly. Its wings are too small to get its fat little body off the ground. The bee, of
-                                    course, flies anyway because bees don't care what humans think is impossible.
-                                    Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and yellow!
-                                    Let's shake it up a little. Barry! Breakfast is ready! Ooming! Hang on a second.
-                                    Hello? - Barry? - Adam? - Oan you believe this is happening? - I can't. I'll pick
-                                    you up. Looking sharp. Use the stairs. Your father paid good money for those. Sorry.
-                                    I'm excited. Here's the graduate. We're very proud of you, son. A perfect report
-                                    card, all B's. Very proud. Ma! I got a thing going here. - You got lint on your
-                                    fuzz. - Ow! That's me! - Wave to us! We'll be in row 118,000. - Bye! Barry, I told
-                                    you, stop flying in the house! - Hey, Adam. - Hey, Barry. - Is that fuzz gel? - A
-                                    little. Special day, graduation. Never thought I'd make it. Three days grade school,
-                                    three days high school. Those were awkward. Three days college. I'm glad I took a
-                                    day and hitchhiked around the hive. You did come back different.
-                                </div>
-                            </BodyPanel>
-                        </Col>
-                        <Col md>
-                            <BodyPanel>
-                                <h5>mememe</h5>
-                                <div>According to all known laws of aviation, there is no way a bee should be able to
-                                    fly. Its wings are too small to get its fat little body off the ground. The bee, of
-                                    course, flies anyway because bees don't care what humans think is impossible.
-                                    Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and yellow!
-                                    Let's shake it up a little. Barry! Breakfast is ready! Ooming! Hang on a second.
-                                    Hello? - Barry? - Adam? - Oan you believe this is happening? - I can't. I'll pick
-                                    you up. Looking sharp. Use the stairs. Your father paid good money for those. Sorry.
-                                    I'm excited. Here's the graduate. We're very proud of you, son. A perfect report
-                                    card, all B's. Very proud. Ma! I got a thing going here. - You got lint on your
-                                    fuzz. - Ow! That's me! - Wave to us! We'll be in row 118,000. - Bye! Barry, I told
-                                    you, stop flying in the house! - Hey, Adam. - Hey, Barry. - Is that fuzz gel? - A
-                                    little. Special day, graduation. Never thought I'd make it. Three days grade school,
-                                    three days high school. Those were awkward. Three days college. I'm glad I took a
-                                    day and hitchhiked around the hive. You did come back different.
-                                </div>
-                            </BodyPanel>
-                        </Col>
+                        <BodyPanel>
+                            <Row>
+                                <Col md>
+                                    <h5>mememe</h5>
+                                    <div>According to all known laws of aviation, there is no way a bee should be able
+                                        to
+                                        fly. Its wings are too small to get its fat little body off the ground. The bee,
+                                        of
+                                        course, flies anyway because bees don't care what humans think is impossible.
+                                        Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and
+                                        yellow!
+                                        Let's shake it up a little. Barry! Breakfast is ready! Ooming! Hang on a second.
+                                        Hello? - Barry? - Adam? - Oan you believe this is happening? - I can't. I'll
+                                        pick
+                                        you up. Looking sharp. Use the stairs. Your father paid good money for those.
+                                        Sorry.
+                                        I'm excited. Here's the graduate. We're very proud of you, son. A perfect report
+                                        card, all B's. Very proud. Ma! I got a thing going here. - You got lint on your
+                                        fuzz. - Ow! That's me! - Wave to us! We'll be in row 118,000. - Bye! Barry, I
+                                        told
+                                        you, stop flying in the house! - Hey, Adam. - Hey, Barry. - Is that fuzz gel? -
+                                        A
+                                        little. Special day, graduation. Never thought I'd make it. Three days grade
+                                        school,
+                                        three days high school. Those were awkward. Three days college. I'm glad I took
+                                        a
+                                        day and hitchhiked around the hive. You did come back different.
+                                    </div>
+                                </Col>
+                                <Col md>
+                                    <img src="https://i.imgur.com/vSD8DGq.gif" alt="regretz"/>
+                                </Col>
+                            </Row>
+                        </BodyPanel>
                     </Row>
                     <Row>
-                        <Col md>
-                            <BodyPanel>
-                                <h5>mememe</h5>
-                                <div>According to all known laws of aviation, there is no way a bee should be able to
-                                    fly. Its wings are too small to get its fat little body off the ground. The bee, of
-                                    course, flies anyway because bees don't care what humans think is impossible.
-                                    Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and yellow!
-                                    Let's shake it up a little. Barry! Breakfast is ready! Ooming! Hang on a second.
-                                    Hello? - Barry? - Adam? - Oan you believe this is happening? - I can't. I'll pick
-                                    you up. Looking sharp. Use the stairs. Your father paid good money for those. Sorry.
-                                    I'm excited. Here's the graduate. We're very proud of you, son. A perfect report
-                                    card, all B's. Very proud. Ma! I got a thing going here. - You got lint on your
-                                    fuzz. - Ow! That's me! - Wave to us! We'll be in row 118,000. - Bye! Barry, I told
-                                    you, stop flying in the house! - Hey, Adam. - Hey, Barry. - Is that fuzz gel? - A
-                                    little. Special day, graduation. Never thought I'd make it. Three days grade school,
-                                    three days high school. Those were awkward. Three days college. I'm glad I took a
-                                    day and hitchhiked around the hive. You did come back different.
-                                </div>
-                            </BodyPanel>
-                        </Col>
-                        <Col md>
-                            <BodyPanel>
-                                <h5>mememe</h5>
-                                <div>According to all known laws of aviation, there is no way a bee should be able to
-                                    fly. Its wings are too small to get its fat little body off the ground. The bee, of
-                                    course, flies anyway because bees don't care what humans think is impossible.
-                                    Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and yellow!
-                                    Let's shake it up a little. Barry! Breakfast is ready! Ooming! Hang on a second.
-                                    Hello? - Barry? - Adam? - Oan you believe this is happening? - I can't. I'll pick
-                                    you up. Looking sharp. Use the stairs. Your father paid good money for those. Sorry.
-                                    I'm excited. Here's the graduate. We're very proud of you, son. A perfect report
-                                    card, all B's. Very proud. Ma! I got a thing going here. - You got lint on your
-                                    fuzz. - Ow! That's me! - Wave to us! We'll be in row 118,000. - Bye! Barry, I told
-                                    you, stop flying in the house! - Hey, Adam. - Hey, Barry. - Is that fuzz gel? - A
-                                    little. Special day, graduation. Never thought I'd make it. Three days grade school,
-                                    three days high school. Those were awkward. Three days college. I'm glad I took a
-                                    day and hitchhiked around the hive. You did come back different.
-                                </div>
-                            </BodyPanel>
-                        </Col>
+                        <BodyPanel>
+                            <Row>
+                                <Col md>
+                                    <img src="https://i.imgur.com/vSD8DGq.gif" alt="regretz"/>
+                                </Col>
+                                <Col md>
+                                    <h5>mememe</h5>
+                                    <div>According to all known laws of aviation, there is no way a bee should be able
+                                        to
+                                        fly. Its wings are too small to get its fat little body off the ground. The bee,
+                                        of
+                                        course, flies anyway because bees don't care what humans think is impossible.
+                                        Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and
+                                        yellow!
+                                        Let's shake it up a little. Barry! Breakfast is ready! Ooming! Hang on a second.
+                                        Hello? - Barry? - Adam? - Oan you believe this is happening? - I can't. I'll
+                                        pick
+                                        you up. Looking sharp. Use the stairs. Your father paid good money for those.
+                                        Sorry.
+                                        I'm excited. Here's the graduate. We're very proud of you, son. A perfect report
+                                        card, all B's. Very proud. Ma! I got a thing going here. - You got lint on your
+                                        fuzz. - Ow! That's me! - Wave to us! We'll be in row 118,000. - Bye! Barry, I
+                                        told
+                                        you, stop flying in the house! - Hey, Adam. - Hey, Barry. - Is that fuzz gel? -
+                                        A
+                                        little. Special day, graduation. Never thought I'd make it. Three days grade
+                                        school,
+                                        three days high school. Those were awkward. Three days college. I'm glad I took
+                                        a
+                                        day and hitchhiked around the hive. You did come back different.
+                                    </div>
+                                </Col>
+                            </Row>
+                        </BodyPanel>
                     </Row>
                 </div>
             </div>
