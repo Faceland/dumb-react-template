c9e314b38dbd18d1718e9b2ab82d5e92f64e99d9
UltraFaceguy <kattreinkyle@gmail.com>
2021-02-28 18:35:35 -0500

    Adding basic equipment tags

--- dumb-template/src/components/Shuffle/ShuffleCollection.js
+++ dumb-template/src/components/Shuffle/ShuffleCollection.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from "react";
 import "./Shuffle.scss"
 import gems from "./gems"
 import tomes from "./tomes"
-
+///
 export const ShuffleCollection = (props) => {
 
     const [filteredItems, setFilteredItems] = useState([{name: "loading", meme: "yes"}])
@@ -13,38 +13,48 @@ export const ShuffleCollection = (props) => {
 
     if (items.length === 0) {
         for (const [key, value] of Object.entries(gems)) {
-            const item = Object.assign({}, value);
+            const item = {};
             item.title = "SOCKET GEM"
             item.name = key;
             item.type = "gem"
-            item.description = value.lore
-            item.img = "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/26/Emerald_JE3_BE3.png"
-            item.background = "#10c810"
-            items.push(item)
+            item.description = value.lore;
+            item.equipment = value["item-groups"].join(" ").toLowerCase()
+            item.img = "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/26/Emerald_JE3_BE3.png";
+            item.background = "#10c810";
+            items.push(item);
         }
 
         for (const [key, value] of Object.entries(tomes)) {
-            const item = Object.assign({}, value);
-            item.title = "ENCHANTMENT TOME"
+            const item = {};
+            item.title = "ENCHANTMENT TOME";
             item.name = key;
-            item.type = "tome"
-            item.description = [value.description]
-            item.img = "https://static.wikia.nocookie.net/minecraft_gamepedia/images/5/50/Book_JE2_BE2.png"
-            item.background = "#1243d9"
-            items.push(item)
+            item.type = "tome";
+            item.description = [value.description];
+            item.equipment = value["item-groups"].join(" ").toLowerCase()
+            item.img = "https://static.wikia.nocookie.net/minecraft_gamepedia/images/5/50/Book_JE2_BE2.png";
+            item.background = "#1243d9";
+            items.push(item);
         }
     }
 
-    useEffect(() => {
-        applyFilters()
-    }, [filter, items, searchText]);
+    const applyFilters = () => {
+        setFilteredItems(items.filter(item => itemMatches(item)));
+    }
 
     const itemMatches = (item) => {
         let match = true;
         for (const prop in filter) {
-            if (match && Object.prototype.hasOwnProperty.call(filter, prop)) {
-                if (filter[prop] && item[prop] !== filter[prop]) {
-                    match = false;
+            if (match && Object.prototype.hasOwnProperty.call(filter, prop) && filter[prop]) {
+                if (filter[prop].strict) {
+                    if (item[prop] !== filter[prop].value) {
+                        match = false;
+                        break;
+                    }
+                } else {
+                    if (item[prop].includes(filter[prop].value)) {
+                        match = false;
+                        break;
+                    }
                 }
             }
         }
@@ -55,12 +65,13 @@ export const ShuffleCollection = (props) => {
         return !searchText ||
             item.name?.toLowerCase().includes(searchText) ||
             item.title?.toLowerCase().includes(searchText) ||
-            item.description?.join().toLowerCase().includes(searchText)
+            item.description?.join(" ").toLowerCase().includes(searchText) ||
+            item.equipment?.includes(searchText)
     }
 
-    const applyFilters = () => {
-        setFilteredItems(items.filter(item => itemMatches(item)));
-    }
+    useEffect(() => {
+        applyFilters()
+    }, [filter, searchText]);
 
     const yeHaplessBuffoon = (
         <div className="shuffleCard" style={{borderColor: "#AA0000"}} key="invalid-search">
@@ -80,22 +91,58 @@ export const ShuffleCollection = (props) => {
     return (
         <div>
             <div>
-                <div>FILTER BY TYPE</div>
-                <button className={`selectorButton-${!filter.type ? "active" : "inactive"}`}
+                <div>FILTER BY EQUIPMENT</div>
+                <button className={`selectorButton-${!filter.equipment?.value ? "active" : "inactive"}`}
                         onClick={() => setFilter({
+                            ...filter,
+                            equipment: undefined
+                        })}>
+                    ANY
+                </button>
+                <button
+                    className={`selectorButton-${filter.equipment?.value === "melee weapon" ? "active" : "inactive"}`}
+                    onClick={() => setFilter({
+                        ...filter,
+                        equipment: {value: "melee weapon", strict: "false"}
+                    })}>
+                    Weapon (Melee)
+                </button>
+                <button
+                    className={`selectorButton-${filter.equipment?.value === "ranged weapon" ? "active" : "inactive"}`}
+                    onClick={() => setFilter({
+                        ...filter,
+                        equipment: {value: "ranged weapon", strict: "false"}
+                    })}>
+                    Weapon (Ranged)
+                </button>
+                <button className={`selectorButton-${filter.equipment?.value === "armor" ? "active" : "inactive"}`}
+                        onClick={() => setFilter({
+                            ...filter,
+                            equipment: {value: "armor", strict: "false"}
+                        })}>
+                    Armor
+                </button>
+            </div>
+            <div>
+                <div>FILTER BY ITEM TYPE</div>
+                <button className={`selectorButton-${!filter.type?.value ? "active" : "inactive"}`}
+                        onClick={() => setFilter({
+                            ...filter,
                             type: undefined
                         })}>
                     ANY
                 </button>
-                <button className={`selectorButton-${filter.type === "gem" ? "active" : "inactive"}`}
+                <button className={`selectorButton-${filter.type?.value === "gem" ? "active" : "inactive"}`}
                         onClick={() => setFilter({
-                            type: "gem"
+                            ...filter,
+                            type: {value: "gem", strict: "true"}
                         })}>
                     SOCKET GEM
                 </button>
-                <button className={`selectorButton-${filter.type === "tome" ? "active" : "inactive"}`}
+                <button className={`selectorButton-${filter.type?.value === "tome" ? "active" : "inactive"}`}
                         onClick={() => setFilter({
-                            type: "tome"
+                            ...filter,
+                            type: {value: "tome", strict: "true"}
                         })}>
                     ENCHANTMENT TOME
                 </button>
@@ -112,6 +159,9 @@ export const ShuffleCollection = (props) => {
                             <div className="shuffleContent">
                                 <div className="title">{item?.title}</div>
                                 <div className="subtitle">{item?.name}</div>
+                                <div>
+                                    <div className="tag">{item?.equipment}</div>
+                                </div>
                             </div>
                             <div className="shuffleContent">
                                 <img src={item?.img} alt="Loading..."/>
