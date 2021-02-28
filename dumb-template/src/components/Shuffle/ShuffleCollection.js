c6bed43ed2d6faddbaee3f8e3fa7caa8d4ed1bdb
UltraFaceguy <kattreinkyle@gmail.com>
2021-02-28 02:22:02 -0500

    More shuffle options

--- dumb-template/src/components/Shuffle/ShuffleCollection.js
+++ dumb-template/src/components/Shuffle/ShuffleCollection.js
@@ -8,6 +8,7 @@ export const ShuffleCollection = (props) => {
     const [items, setItems] = useState([])
     const [filteredItems, setFilteredItems] = useState([])
     const [filter, setFilter] = useState({})
+    const [searchText, setSearchText] = useState(undefined)
 
     useEffect(() => {
         const newItems = [];
@@ -19,6 +20,7 @@ export const ShuffleCollection = (props) => {
             item.type = "gem"
             item.description = value.lore
             item.img = "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/26/Emerald_JE3_BE3.png"
+            item.background = "linear-gradient(#55FF55, #00AA00)"
             newItems.push(item)
         }
 
@@ -29,6 +31,7 @@ export const ShuffleCollection = (props) => {
             item.type = "tome"
             item.description = [value.description]
             item.img = "https://static.wikia.nocookie.net/minecraft_gamepedia/images/5/50/Book_JE2_BE2.png"
+            item.background = "linear-gradient(#5555FF, #0000AA)"
             newItems.push(item)
         }
 
@@ -37,18 +40,25 @@ export const ShuffleCollection = (props) => {
 
     useEffect(() => {
         refilter()
-    }, [filter, items]);
+    }, [filter, items, searchText]);
 
     const itemMatches = (item) => {
         let match = true;
         for (const prop in filter) {
-            if (Object.prototype.hasOwnProperty.call(filter, prop)) {
+            if (match && Object.prototype.hasOwnProperty.call(filter, prop)) {
                 if (filter[prop] && item[prop] !== filter[prop]) {
                     match = false;
                 }
             }
         }
-        return match;
+        return match && itemMatchesSearch(item);
+    }
+
+    const itemMatchesSearch = (item) => {
+        return !searchText ||
+            item.name?.toLowerCase().includes(searchText) ||
+            item.title?.toLowerCase().includes(searchText) ||
+            item.description?.join().toLowerCase().includes(searchText)
     }
 
 
@@ -58,42 +68,52 @@ export const ShuffleCollection = (props) => {
 
     return (
         <div>
-            <div>FILTER BY TYPE</div>
-            <button className={`selectorButton-${!filter.type ? "active" : "inactive"}`}
-                    onClick={() => setFilter({
-                        type: undefined
-                    })}>
-                ANY
-            </button>
-            <button className={`selectorButton-${filter.type === "gem" ? "active" : "inactive"}`}
-                    onClick={() => setFilter({
-                        type: "gem"
-                    })}>
-                SOCKET GEM
-            </button>
-            <button className={`selectorButton-${filter.type === "tome" ? "active" : "inactive"}`}
-                    onClick={() => setFilter({
-                        type: "tome"
-                    })}>
-                ENCHANTMENT TOME
-            </button>
-            <div className="shuffleCards">
-                {filteredItems.map(item =>
-                    <div className="shuffleCard" key={item.name}>
-                        <div className="shuffleContent">
-                            <div>{item.title}</div>
-                            <div>{item.name}</div>
+            <div>
+                <div>FILTER BY TYPE</div>
+                <button className={`selectorButton-${!filter.type ? "active" : "inactive"}`}
+                        onClick={() => setFilter({
+                            type: undefined
+                        })}>
+                    ANY
+                </button>
+                <button className={`selectorButton-${filter.type === "gem" ? "active" : "inactive"}`}
+                        onClick={() => setFilter({
+                            type: "gem"
+                        })}>
+                    SOCKET GEM
+                </button>
+                <button className={`selectorButton-${filter.type === "tome" ? "active" : "inactive"}`}
+                        onClick={() => setFilter({
+                            type: "tome"
+                        })}>
+                    ENCHANTMENT TOME
+                </button>
+            </div>
+            <div>
+                <div>SEARCH</div>
+                <input onChange={(e) => setSearchText(e.target.value.toLowerCase())}/>
+            </div>
+            <div>
+                <div className="shuffleCards">
+                    {filteredItems.map(item =>
+                        <div className="shuffleCardBackground" style={{backgroundImage: `${item.background}`}}>
+                            <div className="shuffleCard" key={item.name}>
+                                <div className="shuffleContent">
+                                    <div>{item.title}</div>
+                                    <div>{item.name}</div>
+                                </div>
+                                <div className="shuffleContent">
+                                    <img src={item.img} alt="aaaaaaaaaa"/>
+                                </div>
+                                <div className="shuffleContent">
+                                    {item.description?.map(line =>
+                                        <p>{line}</p>
+                                    )}
+                                </div>
+                            </div>
                         </div>
-                        <div className="shuffleContent">
-                            <img src={item.img} alt="aaaaaaaaaa"/>
-                        </div>
-                        <div className="shuffleContent">
-                            {item.description?.map(line =>
-                                <p>{line}</p>
-                            )}
-                        </div>
-                    </div>
-                )}
+                    )}
+                </div>
             </div>
         </div>
     );
