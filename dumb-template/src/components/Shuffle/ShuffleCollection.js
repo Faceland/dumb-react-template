6320a88275a2f9b6058b54adb370f2965ce5e5cd
UltraFaceguy <kattreinkyle@gmail.com>
2021-03-01 03:44:56 -0500

    Switching categories over to tags

--- dumb-template/src/components/Shuffle/ShuffleCollection.js
+++ dumb-template/src/components/Shuffle/ShuffleCollection.js
@@ -2,76 +2,122 @@ import React, {useState, useEffect} from "react";
 import "./Shuffle.scss"
 import gems from "./gems"
 import tomes from "./tomes"
-///
+
 export const ShuffleCollection = (props) => {
 
-    const [filteredItems, setFilteredItems] = useState([{name: "loading", meme: "yes"}])
-    const [filter, setFilter] = useState({})
-    const [searchText, setSearchText] = useState(undefined)
+    const [items, setItems] = useState([]);
+    const [filteredItems, setFilteredItems] = useState([{name: "loading", meme: "yes"}]);
+    const [filter, setFilter] = useState({});
+    const [searchText, setSearchText] = useState("");
+    const [searchTags, setSearchTags] = useState([]);
+    const [uniqueTags, setUniqueTags] = useState([]);
 
-    const items = [];
+    useEffect(() => {
+        if (items.length === 0) {
+            const newItems = [];
+            for (const [key, value] of Object.entries(gems)) {
+                const item = {};
+                item.title = "SOCKET GEM"
+                item.name = key;
+                item.type = "gem"
+                item.description = value.lore;
+                item.tags = mapGroupsToTags(value['item-groups'])
+                item.img = "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/26/Emerald_JE3_BE3.png";
+                item.background = "#10c810";
+                newItems.push(item);
+            }
 
-    if (items.length === 0) {
-        for (const [key, value] of Object.entries(gems)) {
-            const item = {};
-            item.title = "SOCKET GEM"
-            item.name = key;
-            item.type = "gem"
-            item.description = value.lore;
-            item.equipment = value["item-groups"].join(" ").toLowerCase()
-            item.img = "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/26/Emerald_JE3_BE3.png";
-            item.background = "#10c810";
-            items.push(item);
+            for (const [key, value] of Object.entries(tomes)) {
+                const item = {};
+                item.title = "ENCHANTMENT TOME";
+                item.name = key;
+                item.type = "tome";
+                item.description = [value.description];
+                item.tags = mapGroupsToTags(value['item-groups'])
+                item.img = "https://static.wikia.nocookie.net/minecraft_gamepedia/images/5/50/Book_JE2_BE2.png";
+                item.background = "#1243d9";
+                newItems.push(item);
+            }
+            setItems(newItems);
+            setSearchTags([]);
         }
+    }, []);
 
-        for (const [key, value] of Object.entries(tomes)) {
-            const item = {};
-            item.title = "ENCHANTMENT TOME";
-            item.name = key;
-            item.type = "tome";
-            item.description = [value.description];
-            item.equipment = value["item-groups"].join(" ").toLowerCase()
-            item.img = "https://static.wikia.nocookie.net/minecraft_gamepedia/images/5/50/Book_JE2_BE2.png";
-            item.background = "#1243d9";
-            items.push(item);
+    useEffect(() => {
+        applyFilters()
+    }, [filter, searchText, searchTags]);
+
+    useEffect(() => {
+        const set = new Set(uniqueTags);
+        if (set.size !== uniqueTags.length) {
+            setUniqueTags(Array.from(set));
         }
+    }, [uniqueTags])
+
+    const mapGroupsToTags = (groups) => {
+        const tags = []
+        groups.forEach((group) => {
+            if (!group.startsWith("-")) {
+                const tag = group.toLowerCase();
+                tags.push(tag);
+                if (!uniqueTags.includes(tag)) {
+                    setUniqueTags(uniqueTags => [...uniqueTags, tag]);
+                }
+            }
+        });
+        console.log("test", tags)
+        return tags;
     }
 
     const applyFilters = () => {
-        setFilteredItems(items.filter(item => itemMatches(item)));
+        setFilteredItems(items.filter(item => itemMatches(item) && itemMatchesSearch(item)));
+    }
+
+    const checkMatch = (property, value, strict) => {
+        return strict ? property === value : property.includes(value);
     }
 
     const itemMatches = (item) => {
         let match = true;
         for (const prop in filter) {
             if (match && Object.prototype.hasOwnProperty.call(filter, prop) && filter[prop]) {
-                if (filter[prop].strict) {
-                    if (item[prop] !== filter[prop].value) {
-                        match = false;
-                        break;
-                    }
-                } else {
-                    if (item[prop].includes(filter[prop].value)) {
-                        match = false;
-                        break;
-                    }
+                if (!checkMatch(item[prop], filter[prop].value, filter[prop].strict)) {
+                    match = false;
+                    break;
                 }
             }
         }
-        return match && itemMatchesSearch(item);
+        return match;
+    }
+
+    const tagsApplicable = (item) => {
+        if (searchTags.length === 0) return true;
+        for (const tag of searchTags) {
+            if (!item.tags.includes(tag)) return false
+        }
+        return true;
     }
 
     const itemMatchesSearch = (item) => {
-        return !searchText ||
+        return tagsApplicable(item) && (!searchText ||
             item.name?.toLowerCase().includes(searchText) ||
             item.title?.toLowerCase().includes(searchText) ||
             item.description?.join(" ").toLowerCase().includes(searchText) ||
-            item.equipment?.includes(searchText)
+            item.tags?.join(" ").toLowerCase().includes(searchText))
     }
 
-    useEffect(() => {
-        applyFilters()
-    }, [filter, searchText]);
+    const toggleTag = (tag) => {
+        const newTags = Object.assign([], searchTags);
+        searchTags.includes(tag) ? newTags.splice(newTags.indexOf(tag), 1) : newTags.push(tag);
+        setSearchTags(newTags);
+    }
+
+    const enableTag = (tag) => {
+        if (searchTags.includes(tag)) return;
+        const newTags = Object.assign([], searchTags);
+        newTags.push(tag);
+        setSearchTags(newTags);
+    }
 
     const yeHaplessBuffoon = (
         <div className="shuffleCard" style={{borderColor: "#AA0000"}} key="invalid-search">
@@ -91,36 +137,16 @@ export const ShuffleCollection = (props) => {
     return (
         <div>
             <div>
-                <div>FILTER BY EQUIPMENT</div>
-                <button className={`selectorButton-${!filter.equipment?.value ? "active" : "inactive"}`}
-                        onClick={() => setFilter({
-                            ...filter,
-                            equipment: undefined
-                        })}>
-                    ANY
-                </button>
-                <button
-                    className={`selectorButton-${filter.equipment?.value === "melee weapon" ? "active" : "inactive"}`}
-                    onClick={() => setFilter({
-                        ...filter,
-                        equipment: {value: "melee weapon", strict: "false"}
-                    })}>
-                    Weapon (Melee)
-                </button>
-                <button
-                    className={`selectorButton-${filter.equipment?.value === "ranged weapon" ? "active" : "inactive"}`}
-                    onClick={() => setFilter({
-                        ...filter,
-                        equipment: {value: "ranged weapon", strict: "false"}
-                    })}>
-                    Weapon (Ranged)
-                </button>
-                <button className={`selectorButton-${filter.equipment?.value === "armor" ? "active" : "inactive"}`}
-                        onClick={() => setFilter({
-                            ...filter,
-                            equipment: {value: "armor", strict: "false"}
-                        })}>
-                    Armor
+                <div>FILTER BY TAG</div>
+                {uniqueTags.map((tag, index) =>
+                    <button
+                        className={`selectorButton-${searchTags.includes(tag) ? "active" : "inactive"}`}
+                        key={"toggle" + index}
+                        onClick={() => toggleTag(tag)}>
+                        {tag}
+                    </button>
+                )}
+                <button type="checkbox" className="selectorButton-clear" onClick={() => setSearchTags([])}>Clear
                 </button>
             </div>
             <div>
@@ -160,7 +186,12 @@ export const ShuffleCollection = (props) => {
                                 <div className="title">{item?.title}</div>
                                 <div className="subtitle">{item?.name}</div>
                                 <div>
-                                    <div className="tag">{item?.equipment}</div>
+                                    {item?.tags?.map((tag, index2) =>
+                                        <button
+                                            className="tag"
+                                            key={`tag${index2}`}
+                                            onClick={() => enableTag(tag)}
+                                        >{tag}</button>)}
                                 </div>
                             </div>
                             <div className="shuffleContent">
