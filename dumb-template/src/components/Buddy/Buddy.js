0744c586ea74c1859caf1858ea470229291d6b74
UltraFaceguy <kattreinkyle@gmail.com>
2021-03-14 20:50:21 -0400

    Removing shake lib and doing it manually

--- dumb-template/src/components/Buddy/Buddy.js
+++ dumb-template/src/components/Buddy/Buddy.js
@@ -1,14 +1,10 @@
-import React, {useContext, useState} from "react";
+import React, {useState} from "react";
 import useSound from "use-sound";
-import { Shake } from 'reshake'
 
 import './buddy.scss'
 import boopSfx from '../../sounds/Player_hurt1.ogg';
-import {Context} from "../../Store";
 
 export const Buddy = () => {
-
-    const [state] = useContext(Context);
 
     const [life, setLife] = useState(5);
     const [shake, setShake] = useState(false)
@@ -28,32 +24,20 @@ export const Buddy = () => {
     }
 
 
-    if (state.mobile || life < 1) {
+    if (life < 1) {
         return <></>;
     }
 
     return (
-        <Shake
-            h={5}
-            v={5}
-            r={3}
-            dur={300}
-            int={10}
-            max={100}
-            fixed={false}
-            fixedStop={false}
-            freez={false}
-            active={shake}>
-            <img
-                className="buddy"
-                src="https://i.imgur.com/2kT4dcX.png"
-                alt="facebuddy"
-                onClick={() => {
-                    doShake()
-                    playSound();
-                    setLife(life - 1);
-                }}
-            />
-        </Shake>
+        <img
+            className={`buddy ${shake ? "hitShake" : ""}`}
+            src="https://i.imgur.com/2kT4dcX.png"
+            alt="facebuddy"
+            onClick={() => {
+                doShake()
+                playSound();
+                setLife(life - 1);
+            }}
+        />
     );
 }
