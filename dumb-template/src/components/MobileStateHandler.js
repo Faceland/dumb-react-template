fb54177863ab1a77f4cbfe2f1a40232a1f2be5ba
UltraFaceguy <kattreinkyle@gmail.com>
2021-03-16 23:37:15 -0400

    Fixing mobile state on iOS because they're dumb

--- dumb-template/src/components/MobileStateHandler.js
+++ dumb-template/src/components/MobileStateHandler.js
@@ -6,11 +6,11 @@ export const MobileStateHandler = () => {
     const [state, dispatch] = useContext(Context);
 
     window.onresize = function () {
-        dispatch({type: 'SET_MOBILE_MODE', payload: window.innerHeight / window.innerWidth > 1.5})
+        dispatch({type: 'SET_MOBILE_MODE', payload: window.screen.height / window.screen.width > 1.5})
     }
 
     useEffect(() => {
-        dispatch({type: 'SET_MOBILE_MODE', payload: window.innerHeight / window.innerWidth > 1.5})
+        dispatch({type: 'SET_MOBILE_MODE', payload: window.screen.height / window.screen.width > 1.5})
     }, []);
 
     return <div/>
